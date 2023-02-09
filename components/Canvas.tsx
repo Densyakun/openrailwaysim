287977c17c1ad84dc99309f84af21eaf0c6c02b5
densyakun <densyakun1128@gmail.com>
2023-02-09 21:21:23 +0000

    sky and shadow test

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -1,10 +1,24 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas, Vector3 } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { Sky } from '@react-three/drei'
 
 export default function App() {
+  const sunPosition: Vector3 = [1, 2, 3]
+
   return (
     <div id="canvas-container">
-      <Canvas>
-        <mesh>
+      <Canvas shadows>
+        <ambientLight intensity={0.1} />
+        <directionalLight castShadow position={sunPosition} />
+        <OrbitControls />
+        <Sky sunPosition={sunPosition} />
+        <mesh receiveShadow>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial />
+        </mesh>
+        <mesh castShadow position={[1, 2, 3]}>
+          <boxGeometry />
+          <meshStandardMaterial />
         </mesh>
       </Canvas>
     </div>

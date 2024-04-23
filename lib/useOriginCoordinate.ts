bed9da817b8e1f20e908cec2a3ad202e943329a1
Densyakun <densyakun1128@gmail.com>
2024-04-23 20:44:12 +0900

    useOriginCoordinateの更新頻度を抑える

--- lib/useOriginCoordinate.ts
+++ lib/useOriginCoordinate.ts
@@ -2,11 +2,16 @@ import * as React from 'react'
 import { useFrame } from '@react-three/fiber'
 import { getOriginEuler, eulerToCoordinate } from '@/lib/gis'
 
+//const a = 1
+
 export default function useOriginCoordinate() {
   const [originCoordinate, setOriginCoordinate] = React.useState(eulerToCoordinate(getOriginEuler()))
 
   useFrame(() => {
-    setOriginCoordinate(eulerToCoordinate(getOriginEuler()))
+    const newCoordinate = eulerToCoordinate(getOriginEuler())
+    if (originCoordinate[0]/* * a*/ >> 0 !== newCoordinate[0]/* * a*/ >> 0
+      || originCoordinate[1]/* * a*/ >> 0 !== newCoordinate[1]/* * a*/ >> 0)
+      setOriginCoordinate(newCoordinate)
   })
 
   return originCoordinate

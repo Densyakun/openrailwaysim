8e42684e6d7bc7b119d271864ad9be5e0e63e7e8
Densyakun <densyakun1128@gmail.com>
2024-12-02 20:08:21 +0900

    ボギーが選択された状態で選択を解除するボタンを追加

--- components/hud/ControlStand.tsx
+++ components/hud/ControlStand.tsx
@@ -3,6 +3,8 @@ import { useSnapshot } from 'valtio';
 import { state as trainsState } from '@/lib/trains';
 import MasterController from './MasterController';
 import Speed from './Speed';
+import { Button } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function ControlStand() {
   useSnapshot(trainsState);
@@ -13,6 +15,12 @@ export default function ControlStand() {
     <>
       <MasterController />
       <Speed />
+      <Button variant='contained' startIcon={<CloseIcon />} onClick={() => {
+        trainsState.activeBodyIndex = -1
+        trainsState.activeTrainId = ""
+      }}>
+        Back
+      </Button>
     </>
   );
 }

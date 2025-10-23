afb109c4cb2ff7f510551497909d890d4313309a
Densyakun <densyakun1128@gmail.com>
2025-10-24 02:07:27 +0900

    GLTFのグループとその子孫メッシュに対応

--- src/components/GLTFModel.tsx
+++ src/components/GLTFModel.tsx
@@ -1,23 +1,47 @@
 import * as THREE from 'three';
 import { useGLTF } from "@react-three/drei";
-import { MeshProps } from "@react-three/fiber";
+import { GroupProps, MeshProps } from "@react-three/fiber";
 
 export default function GLTFModel({ modelPath, meshProps }: { modelPath: string, meshProps?: MeshProps }) {
   const gltf = useGLTF(modelPath);
 
-  return <>
-    {gltf.scene.children.map((child, index) => (
-      <mesh
-        key={index}
-        castShadow
-        receiveShadow
-        position={(child as THREE.Mesh).position}
-        rotation={(child as THREE.Mesh).rotation}
-        scale={(child as THREE.Mesh).scale}
-        geometry={(child as THREE.Mesh).geometry}
-        material={(child as THREE.Mesh).material}
-        {...meshProps}
-      />
-    ))}
-  </>;
+  return <Group
+    childrenObjects={gltf.scene.children}
+    meshProps={meshProps}
+  />;
+}
+
+function Mesh(props: MeshProps) {
+  return <mesh {...props} />;
+}
+
+function Group(props: GroupProps & { childrenObjects: THREE.Object3D[], meshProps?: MeshProps }) {
+  return <group {...props}>
+    {props.childrenObjects.map((child, index) => {
+      if (child.type === "Mesh")
+        return <Mesh
+          key={index}
+          castShadow
+          receiveShadow
+          position={(child as THREE.Mesh).position}
+          rotation={(child as THREE.Mesh).rotation}
+          scale={(child as THREE.Mesh).scale}
+          geometry={(child as THREE.Mesh).geometry}
+          material={(child as THREE.Mesh).material}
+          {...props.meshProps}
+        />;
+
+      if (child.type === "Group")
+        return <Group
+          key={index}
+          position={(child as THREE.Mesh).position}
+          rotation={(child as THREE.Mesh).rotation}
+          scale={(child as THREE.Mesh).scale}
+          childrenObjects={child.children}
+          meshProps={props.meshProps}
+        />;
+
+      return null;
+    })}
+  </group>;
 }
